636fd01688756b6f4caf938f0968119066bf3b1c
Ajkunas <ajkuna.seipi@epfl.ch>
2023-05-02 19:09:27 +0200

    update button style

--- docs/barplot.js
+++ docs/barplot.js
@@ -1,66 +1,88 @@
-// set the dimensions and margins of the graph
-const container_w1 = 1200;
-const container_h1 = 400;
-
-const width1 = 300;
-const height1 = 300;
-
-const marginL = 80;
-const marginT = 30;
+ // set the dimensions and margins of the graph
+ var margin = {top: 60, right: 30, bottom: 70, left: 50},
+ width1 = 1200 - margin.left - margin.right,
+ height1 = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#barplot")
 .append("svg")
- .attr("width", container_w1)
- .attr("height", container_h1)
+ .attr("width", width1 + margin.left + margin.right)
+ .attr("height", height1 + margin.top + margin.bottom)
 .append("g")
  .attr("transform",
-	   "translate(" + marginL + "," + marginT + ")");
+	   "translate(" + margin.left + "," + margin.top + ")");
 
 
 // Parse the Data
-// raw path "https://github.com/com-480-data-visualization/project-2023-matchmakers/blob/master/data/average_interests.csv"
-// var data_path = "https://github.com/com-480-data-visualization/project-2023-matchmakers/blob/master/data/average_interests.csv";
-var dpath = "data/average_interests.csv";
-d3.csv(dpath, {
-  delimiter: ",",
-  header: true
-}).then(function(data) {
+//var dpath = "https://github.com/com-480-data-visualization/project-2023-matchmakers/blob/master/data/average_interests.csv";
 
   // X axis
   var x = d3.scaleBand()
     .range([ 0, width1 ])
-    .domain(data.interests.map(function(d) {
-		console.log(d)
-		 return d.interests; }))
     .padding(0.2);
-  svg.append("g")
+
+  var xAxis = svg.append("g")
     .attr("transform", "translate(0," + height1 + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+  
+  
+  //  svg.append("g")
+  //  .attr("transform", "translate(0," + height1 + ")")
+   // .call(d3.axisBottom(x))
+   // .selectAll("text")
+   //   .attr("transform", "translate(-10,0)rotate(-45)")
+  //    .style("text-anchor", "end");
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 10])
     .range([ height1, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
 
-  // Bars
-  svg.selectAll("mybar")
-    .data(data)
-    .enter()
-    .append("rect")
-      .attr("x", function(d) {
-		console.log(d.interests)
-		return x(d.interests); })
-      .attr("y", function(d) {
-		console.log(d.female)
-		return y(d.female); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height1 - y(d.interests); })
-      .attr("fill", "#b36989")
+  var yAxis = svg.append("g")
+    .attr("class", "myYaxis")
 
-})
+
+  function update(selectedVar) {
+    var dpath = "data/average_interests.csv";
+    d3.csv(dpath, {
+      delimiter: ",",
+      header: true
+    }).then(function(data) {
+
+      // X axis
+    x.domain(data.map(function(d) { return d.interests; }))
+    xAxis.transition().duration(1000).call(d3.axisBottom(x))
+
+    // Add Y axis
+    y.domain([0, d3.max(data, function(d) { return +d[selectedVar] }) ]);
+    yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+    // variable u: map data to existing bars
+    var u = svg.selectAll("rect")
+      .data(data)
+
+    // update bars
+    var color = "#e84388"
+
+    if (selectedVar == "female") {
+      color = "#e84388"
+    } else {
+      color = "#4349e8"
+    }
+
+    u
+      .enter()
+      .append("rect")
+      .merge(u)
+      .transition()
+      .duration(1000)
+        .attr("x", function(d) { return x(d.interests); })
+        .attr("y", function(d) { return y(d[selectedVar]); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height1 - y(d[selectedVar]); })
+        .attr("fill", color)
+  })
+
+}
+
+// This function is called by the buttons on top of the plot
+
+update('female')
