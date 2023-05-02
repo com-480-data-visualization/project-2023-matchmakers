fe482f125a871ab89fd71a83c9960d632f3c6d6a
Ajkunas <ajkuna.seipi@epfl.ch>
2023-05-02 12:36:12 +0200

    update barplot

--- docs/barplot.js
+++ docs/barplot.js
@@ -1,29 +1,27 @@
-// set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-width = 460 - margin.left - margin.right,
-height = 400 - margin.top - margin.bottom;
+ // set the dimensions and margins of the graph
+ var margin = {top: 30, right: 30, bottom: 70, left: 60},
+ width = 460 - margin.left - margin.right,
+ height = 400 - margin.top - margin.bottom;
 
-// append the svg obZject to the body of the page
-var svg = d3.select("#barplot")
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
 .append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
+ .attr("width", width + margin.left + margin.right)
+ .attr("height", height + margin.top + margin.bottom)
 .append("g")
-.attr("transform",
-	  "translate(" + margin.left + "," + margin.top + ")");
+ .attr("transform",
+	   "translate(" + margin.left + "," + margin.top + ")");
 
 // Initialize the X axis
 var x = d3.scaleBand()
 .range([ 0, width ])
 .padding(0.2);
-
 var xAxis = svg.append("g")
 .attr("transform", "translate(0," + height + ")")
 
 // Initialize the Y axis
 var y = d3.scaleLinear()
 .range([ height, 0]);
-
 var yAxis = svg.append("g")
 .attr("class", "myYaxis")
 
@@ -31,44 +29,35 @@ var yAxis = svg.append("g")
 // A function that create / update the plot for a given variable:
 function update(selectedVar) {
 
-	// Parse the Data
-	d3.csv("data/average_interests.csv", function(data) {
+// Parse the Data
+// raw path
+d3.csv("https://github.com/com-480-data-visualization/project-2023-matchmakers/blob/master/data/average_interests.csv", function(data) {
 
-		for (var i = 0; i < data.length; i++) {
-			console.log(data[i].interests);
-			console.log(data[i].male);
-		}
+ // X axis
+ x.domain(data.map(function(d) { return d.interests; }))
+ xAxis.transition().duration(1000).call(d3.axisBottom(x))
 
-	// X axis
-	x.domain(data.map(function(d) { return d.interests; }))
-	xAxis.transition().duration(1000).call(d3.axisBottom(x))
+ // Add Y axis
+ y.domain([0, d3.max(data, function(d) { return +d[selectedVar] }) ]);
+ yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
-	// Add Y axis
-	y.domain([0, d3.max(data, function(d) { return +d[selectedVar] }) ]);
-	yAxis.transition().duration(1000).call(d3.axisLeft(y));
+ // variable u: map data to existing bars
+ var u = svg.selectAll("rect")
+   .data(data)
 
-	// variable u: map data to existing bars
-	var u = svg.selectAll("rect")
-	.data(data)
-
-	// update bars
-	u
-	.enter()
-	.append("rect")
-	.merge(u)
-	.transition()
-	.duration(1000)
-		.attr("x", function(d) { return x(d.Interests); })
-		.attr("y", function(d) { return y(d[selectedVar]); })
-		.attr("width", x.bandwidth())
-		.attr("height", function(d) { return height - y(d[selectedVar]); })
-
-		if (selectedVar == 'female') {
-			u.attr("fill", "#b36999")
-		} else {
-			u.attr("fill", "#6998b3")
-		}
-	})
+ // update bars
+ u
+   .enter()
+   .append("rect")
+   .merge(u)
+   .transition()
+   .duration(1000)
+	 .attr("x", function(d) { return x(d.interests); })
+	 .attr("y", function(d) { return y(d[selectedVar]); })
+	 .attr("width", x.bandwidth())
+	 .attr("height", function(d) { return height - y(d[selectedVar]); })
+	 .attr("fill", "#69b3a2")
+})
 
 }
 
