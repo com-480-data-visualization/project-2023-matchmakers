4d82562aae147e27f7caa2a0f5f96ed8ca5cd8a0
Ajkunas <ajkuna.seipi@epfl.ch>
2023-05-02 13:14:51 +0200

    update barplot

--- docs/barplot.js
+++ docs/barplot.js
@@ -1,9 +1,9 @@
  // set the dimensions and margins of the graph
- const container_w = 1200;
- const container_h = 400;
+ const container_w1 = 1200;
+ const container_h1 = 400;
  
- const width = 300;
- const height = 300;
+ const width1 = 300;
+ const height1 = 300;
 
  const marginL = 80;
 const marginT = 30;
@@ -11,8 +11,8 @@ const marginT = 30;
 // append the svg object to the body of the page
 var svg = d3.select("#barplot")
 .append("svg")
- .attr("width", container_w)
- .attr("height", container_h)
+ .attr("width", container_w1)
+ .attr("height", container_h1)
 .append("g")
  .attr("transform",
 	   "translate(" + marginL + "," + marginT + ")");
@@ -24,11 +24,11 @@ d3.csv("https://github.com/com-480-data-visualization/project-2023-matchmakers/b
 
   // X axis
   var x = d3.scaleBand()
-    .range([ 0, width ])
+    .range([ 0, width1 ])
     .domain(data.map(function(d) { return d.interests; }))
     .padding(0.2);
   svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
+    .attr("transform", "translate(0," + height1 + ")")
     .call(d3.axisBottom(x))
     .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
@@ -37,7 +37,7 @@ d3.csv("https://github.com/com-480-data-visualization/project-2023-matchmakers/b
   // Add Y axis
   var y = d3.scaleLinear()
     .domain([0, 13000])
-    .range([ height, 0]);
+    .range([ height1, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
@@ -49,7 +49,7 @@ d3.csv("https://github.com/com-480-data-visualization/project-2023-matchmakers/b
       .attr("x", function(d) { return x(d.interests); })
       .attr("y", function(d) { return y(d.female); })
       .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.interests); })
+      .attr("height", function(d) { return height1 - y(d.interests); })
       .attr("fill", "#b36989")
 
 })
