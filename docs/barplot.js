ee6d4855085910e301b1a986f5572a07da244848
Ajkunas <ajkuna.seipi@epfl.ch>
2023-05-02 13:02:15 +0200

    update barplot

--- docs/barplot.js
+++ docs/barplot.js
@@ -12,54 +12,39 @@ var svg = d3.select("#barplot")
  .attr("transform",
 	   "translate(" + margin.left + "," + margin.top + ")");
 
-// Initialize the X axis
-var x = d3.scaleBand()
-.range([ 0, width ])
-.padding(0.2);
-var xAxis = svg.append("g")
-.attr("transform", "translate(0," + height + ")")
-
-// Initialize the Y axis
-var y = d3.scaleLinear()
-.range([ height, 0]);
-var yAxis = svg.append("g")
-.attr("class", "myYaxis")
-
-
-// A function that create / update the plot for a given variable:
-function update(selectedVar) {
 
 // Parse the Data
-// raw path
+// raw path "https://github.com/com-480-data-visualization/project-2023-matchmakers/blob/master/data/average_interests.csv"
 d3.csv("https://github.com/com-480-data-visualization/project-2023-matchmakers/blob/master/data/average_interests.csv", function(data) {
 
- // X axis
- x.domain(data.map(function(d) { return d.interests; }))
- xAxis.transition().duration(1000).call(d3.axisBottom(x))
+  // X axis
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(function(d) { return d.interests; }))
+    .padding(0.2);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
- // Add Y axis
- y.domain([0, d3.max(data, function(d) { return +d[selectedVar] }) ]);
- yAxis.transition().duration(1000).call(d3.axisLeft(y));
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 13000])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
- // variable u: map data to existing bars
- var u = svg.selectAll("rect")
-   .data(data)
+  // Bars
+  svg.selectAll("mybar")
+    .data(data)
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.interests); })
+      .attr("y", function(d) { return y(d.female); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.interests); })
+      .attr("fill", "#b36989")
 
- // update bars
- u
-   .enter()
-   .append("rect")
-   .merge(u)
-   .transition()
-   .duration(1000)
-	 .attr("x", function(d) { return x(d.interests); })
-	 .attr("y", function(d) { return y(d[selectedVar]); })
-	 .attr("width", x.bandwidth())
-	 .attr("height", function(d) { return height - y(d[selectedVar]); })
-	 .attr("fill", "#69b3a2")
 })
-
-}
-
-// Initialize plot
-update('female')
