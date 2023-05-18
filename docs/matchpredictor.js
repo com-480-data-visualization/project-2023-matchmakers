4cdd9a852102521a9b9bc41f44dc24e8e0081dd6
Nat998 <31179705+Nat998@users.noreply.github.com>
2023-05-18 12:48:31 +0200

    M3: match model update + check

--- docs/matchpredictor.js
+++ docs/matchpredictor.js
@@ -1,7 +1,7 @@
-import * as tfcore from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-core";
+/*import * as tfcore from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-core";
 import * as tfcpu from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-cpu";
 import * as tfconv from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-converter";
-import * as tfdf from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tfdf/dist/tf-tfdf.min.js";
+import * as tfdf from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tfdf/dist/tf-tfdf.min.js"; */
 
 
 const colors_map = {"female":"deeppink", "male":"blue", "all":"darkorange"};
@@ -14,10 +14,18 @@ height1 = 400 - margin.top - margin.bottom;
 
 const data_path = "data/people.csv";
 
-tfdfModel
+//
+const model = await tf.loadGraphModel('matchmodel/model.json')
+const xs = tf.tensor2d([ 1.06318785,  1.33890233,  1.46240333, -1.04112199,  0.35454676,
+        2.59838526, -1.1037124 ,  0.04642716, -0.16618421, -1.05857994,
+        0.37055393, -0.08550185, -1.10840028,  0.04145437,  2.74283214,
+       -1.00297304, -0.22643017, -1.09902919, -0.32037796, -0.27160724,
+       -0.22694717,  0.93111942, -0.31674382, -0.27160724], [1, 24]);
 
-//const tfdfModel = await tfdf.loadTFDFModel('match_model');
-
+console.log(model);
+console.log(xs);
+let pred = await model.executeAsync(xs);
+console.log('pred:${predDocument}');
 /*clf2 = DecisionTreeClassifier()
 clf3 = GaussianNB()
 
