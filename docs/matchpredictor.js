2b890b36364288df31f186106dbdb38d36cb817b
Nat998 <31179705+Nat998@users.noreply.github.com>
2023-05-18 15:04:00 +0200

    M3: more model stuff

--- docs/matchpredictor.js
+++ docs/matchpredictor.js
@@ -1,86 +1,56 @@
 const colors_map = {"female":"deeppink", "male":"blue", "all":"darkorange"};
 const labels_map = {"female":"Women", "male":"Men", "all":"Everyone"};
 
-(async () => {
- // Load the model.
- // Tensorflow.js currently needs the absolute path to the model including the full origin.
- //const model = await tfdf.loadTFDFModel('http://127.0.0.1:8080/project-2023-matchmakers/matchmodel/model.json');
- const model = await tfdf.loadTFDFModel('matchmodel/model.json');
- // Perform an inference
- const res = await model.executeAsync(tf.tensor([[ 1.06318785,  1.33890233,  1.46240333, -1.04112199,  0.35454676,
-  2.59838526, -1.1037124 ,  0.04642716, -0.16618421, -1.05857994,
-  0.37055393, -0.08550185, -1.10840028,  0.04145437,  2.74283214,
- -1.00297304, -0.22643017, -1.09902919, -0.32037796, -0.27160724,
- -0.22694717,  0.93111942, -0.31674382, -0.27160724]]));
- console.log(typeof res);
- res.print();
-/* const result = await model.executeAsync({
-    "age":tf.tensor([1.063188]),
-    "age_o":tf.tensor([1.338902]),
-    "interests_correlate":tf.tensor([1.462403]),
-    "pref_o_attractive":tf.tensor([-1.041122]),
-    "pref_o_sincere":tf.tensor([0.354547]),
-    "pref_o_intelligence":tf.tensor ([2.598385]),
-    "pref_o_funny":tf.tensor([-1.103712]),
-    "pref_o_ambitious":tf.tensor([0.046427]),
-    "pref_o_shared_interests":tf.tensor([-0.166184]),
-    "attractive_important":tf.tensor([-1.058580]),
-    "sincere_important":tf.tensor([0.370554]),
-    "intellicence_important":tf.tensor([-0.085502]),
-    "funny_important":tf.tensor([-1.108400]),
-    "ambtition_important":tf.tensor([0.041454]),
-    "shared_interests_important":tf.tensor([2.742832]),
-    "gender_male":tf.tensor([-1.002973]),
-    "race_Black":tf.tensor([-0.226430]),
-    "race_Caucasian" :tf.tensor([-1.099029]),
-    "race_Latinx"   :tf.tensor([-0.320378]),
-    "race_Other"  :tf.tensor([-0.271607]),
-    "race_o_Black"  :tf.tensor([-0.226947]),
-    "race_o_Caucasian"  :tf.tensor([0.931119]),
-    "race_o_Latinx":tf.tensor([-0.316744]),
-    "race_o_Other":tf.tensor([-0.271607]),
- });
- // The result is a 6-dimensional vector, the first half may be ignored
- result.print();*/
-})();
-/*
-// set the dimensions and margins of the graph
-var margin = {top: 60, right: 30, bottom: 70, left: 30},
-width1 = 1200 - margin.left - margin.right,
-height1 = 400 - margin.top - margin.bottom;
 
-const data_path = "data/people.csv";
+const button = document.getElementById("predict-button");
 
-//
-const model = await tf.loadGraphModel('matchmodel/model.json')
-const xs = tf.tensor2d([ 1.06318785,  1.33890233,  1.46240333, -1.04112199,  0.35454676,
-        2.59838526, -1.1037124 ,  0.04642716, -0.16618421, -1.05857994,
-        0.37055393, -0.08550185, -1.10840028,  0.04145437,  2.74283214,
-       -1.00297304, -0.22643017, -1.09902919, -0.32037796, -0.27160724,
-       -0.22694717,  0.93111942, -0.31674382, -0.27160724], [1, 24]);
+button.addEventListener("click", async() => {
+  //const model = await tfdf.loadTFDFModel('http://127.0.0.1:8080/project-2023-matchmakers/matchmodel/model.json');
+  const model = await tfdf.loadTFDFModel('http://project-2023-matchmakers/docs/matchmodel/model.json');
 
-console.log(model);
-console.log(xs);
-let pred = await model.executeAsync(xs);
-console.log('pred:${predDocument}');
-/*clf2 = DecisionTreeClassifier()
-clf3 = GaussianNB()
+  let age = parseFloat(document.getElementById("age").value);
+  let age_o = parseFloat(document.getElementById("age_o").value);
 
+  let att = parseFloat(document.getElementById("att").value);
+  let sincere = parseFloat(document.getElementById("sincere").value);
+  let intell = parseFloat(document.getElementById("intell").value);
+  let fun = parseFloat(document.getElementById("fun").value);
+  let ambit = parseFloat(document.getElementById("ambit").value);
+  let shared = parseFloat(document.getElementById("shared").value);
 
-clf = VotingClassifier(estimators=[
-        ('lr', clf1), ('dt', clf2), ('gnb', clf3)],
-        voting='soft',
-        flatten_transform=True)
-clf = clf.fit(X_train, y_train, train_weights) */
+  let att_o = parseFloat(document.getElementById("att_o").value);
+  let sincere_o = parseFloat(document.getElementById("sincere_o").value);
+  let intell_o = parseFloat(document.getElementById("intell_o").value);
+  let fun_o = parseFloat(document.getElementById("fun_o").value);
+  let ambit_o = parseFloat(document.getElementById("ambit_o").value);
+  let shared_o = parseFloat(document.getElementById("shared_o").value);
 
+  let race = document.getElementById("race").value;
+  let race_o = document.getElementById("race_o").value;
 
-console.log('match predictor script');
+  //let interests_corr = 1.462403;
+  let interests_corr = -1;
 
-/*
-d3.csv(data_path, {
-  delimiter: ",",
-  header: true
-}).then(function(dataa){
+  let wh = race=="white" ? 1 : 0;
+  let bl = race=="black" ? 1 : 0;
+  let lat = race=="latinx" ? 1 : 0;
+  let asian = race=="asian" ? 1 : 0;
 
+  let wh_o = race=="white" ? 1 : 0;
+  let bl_o = race=="black" ? 1 : 0;
+  let lat_o = race=="latinx" ? 1 : 0;
+  let o_o = race=="asian" ? 1 : 0;
+
+  let inp = tf.tensor([[
+    age, age_o, interests_corr,
+    att_o, sincere_o, intell_o, fun_o, ambit_o, shared_o,
+    att, sincere, intell, fun, ambit, shared,
+    -1.002973,
+    wh, bl, lat, asian,
+    wh_o, bl_o, lat_o, o_o
+  ]]);
+
+  let matchproba = await model.executeAsync(inp);
+
+  console.log(matchproba.arraySync()[0][0]);
 })
-*/
