53322d401a774bfd2feab362a9ea4a6951387405
Nat998 <31179705+Nat998@users.noreply.github.com>
2023-05-18 12:52:11 +0200

    M3: path to model edit

--- docs/matchpredictor.js
+++ docs/matchpredictor.js
@@ -1,12 +1,49 @@
-/*import * as tfcore from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-core";
-import * as tfcpu from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-cpu";
-import * as tfconv from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-converter";
-import * as tfdf from "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tfdf/dist/tf-tfdf.min.js"; */
-
-
 const colors_map = {"female":"deeppink", "male":"blue", "all":"darkorange"};
 const labels_map = {"female":"Women", "male":"Men", "all":"Everyone"};
 
+(async () => {
+ // Load the model.
+ // Tensorflow.js currently needs the absolute path to the model including the full origin.
+ //const model = await tfdf.loadTFDFModel('http://127.0.0.1:8080/project-2023-matchmakers/matchmodel/model.json');
+ const model = await tfdf.loadTFDFModel('matchmodel/model.json');
+ // Perform an inference
+ const res = await model.executeAsync(tf.tensor([[ 1.06318785,  1.33890233,  1.46240333, -1.04112199,  0.35454676,
+  2.59838526, -1.1037124 ,  0.04642716, -0.16618421, -1.05857994,
+  0.37055393, -0.08550185, -1.10840028,  0.04145437,  2.74283214,
+ -1.00297304, -0.22643017, -1.09902919, -0.32037796, -0.27160724,
+ -0.22694717,  0.93111942, -0.31674382, -0.27160724]]));
+ console.log(typeof res);
+ res.print();
+/* const result = await model.executeAsync({
+    "age":tf.tensor([1.063188]),
+    "age_o":tf.tensor([1.338902]),
+    "interests_correlate":tf.tensor([1.462403]),
+    "pref_o_attractive":tf.tensor([-1.041122]),
+    "pref_o_sincere":tf.tensor([0.354547]),
+    "pref_o_intelligence":tf.tensor ([2.598385]),
+    "pref_o_funny":tf.tensor([-1.103712]),
+    "pref_o_ambitious":tf.tensor([0.046427]),
+    "pref_o_shared_interests":tf.tensor([-0.166184]),
+    "attractive_important":tf.tensor([-1.058580]),
+    "sincere_important":tf.tensor([0.370554]),
+    "intellicence_important":tf.tensor([-0.085502]),
+    "funny_important":tf.tensor([-1.108400]),
+    "ambtition_important":tf.tensor([0.041454]),
+    "shared_interests_important":tf.tensor([2.742832]),
+    "gender_male":tf.tensor([-1.002973]),
+    "race_Black":tf.tensor([-0.226430]),
+    "race_Caucasian" :tf.tensor([-1.099029]),
+    "race_Latinx"   :tf.tensor([-0.320378]),
+    "race_Other"  :tf.tensor([-0.271607]),
+    "race_o_Black"  :tf.tensor([-0.226947]),
+    "race_o_Caucasian"  :tf.tensor([0.931119]),
+    "race_o_Latinx":tf.tensor([-0.316744]),
+    "race_o_Other":tf.tensor([-0.271607]),
+ });
+ // The result is a 6-dimensional vector, the first half may be ignored
+ result.print();*/
+})();
+/*
 // set the dimensions and margins of the graph
 var margin = {top: 60, right: 30, bottom: 70, left: 30},
 width1 = 1200 - margin.left - margin.right,
